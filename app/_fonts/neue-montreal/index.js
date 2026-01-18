4042a4286b59a4cfd679f56c4ae2e3dcddbfc797
Zedd RM <rafaelmedradomiranda@gmail.com>
2026-01-18 18:17:18 -0300

    first commit

---
+++ app/_fonts/neue-montreal/index.js
@@ -0,0 +1,9 @@
+import localFont from 'next/font/local';
+
+export const neue_montreal = localFont({
+  src: './regular/index.woff2',
+  weight: '500',
+  style: '500',
+  variable: '--font-neue-montreal',
+  display: 'swap',
+});
